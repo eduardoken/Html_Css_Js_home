24f74b169834adc6724eba56dc2ea7d43d438cb1
Eduardo Kenzo <edukenzo428@gmail.com>
2026-02-20 16:05:05 -0300

    operadores logicos

---
+++ fundamentos-js/operadores-logicos/index.js
@@ -0,0 +1,55 @@
+// == operador de igual, leva em consideração apenas o valor da variável e não o tipo dela
+
+// const numero1 = 3
+// const numero2 = "3"
+
+// console.log(numero1 == numero2)
+
+// === operador de indêntico, leva em consideração tanto o tipo quanto o valor
+
+// const numero1 = 3
+// const numero2 = "3"
+
+// console.log(numero1 === numero2)
+
+// // !== não indentico, testa se as variáveis são diferentes em valo e tipo
+
+// const numero1 = 3
+// const numero2 = "3"
+
+// console.log(numero1 !== numero2)
+
+// !== não indentico, testa se as variáveis são diferentes em valor sem considerar o tipo
+
+// const numero1 = 3
+// const numero2 = "3"
+
+// console.log(numero1 != numero2)
+
+// menor que e menor ou igual
+
+// const numero1 = 3
+// const numero2 = 5
+
+// console.log(numero1 <= numero2)
+
+// //maior que e maior ou igual
+
+// const numero1 = 3
+// const numero2 = 5
+
+// console.log(numero1 >= numero2)
+
+//menor que
+
+// const numero1 = 3
+// const numero2 = 5
+
+// console.log(numero1 < numero2)
+
+//maior que
+
+const numero1 = 3
+const numero2 = 5
+
+console.log(numero1 > numero2)
