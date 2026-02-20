5a015bc91c0c6435db65900db4afd4ea51829754
Eduardo Kenzo <edukenzo428@gmail.com>
2026-02-20 16:21:24 -0300

    operadores2

---
+++ fundamentos-js/operadores-logicos-e-ou-nao/index.js
@@ -0,0 +1,28 @@
+// && (AND) - true se ambos os operadores forem verdadeiros
+
+// const numero1 = 2
+// const numero2 = 2
+
+// console.log(numero1 === numero2 && numero1 <= numero2)
+
+// || (OR) - true se pelo menos um dos operandos for verdadeiro
+
+// const numero1 = 4
+// const numero2 = 3
+
+// console.log(numero1 === numero2 || numero1 <= numero2)
+
+// ! (Not) - true se o operando for false
+
+let hasError = true
+console.log(hasError)
+
+if (hasError){
+    console.log("ocorreu um erro")
+}
+
+// console.log(!hasError)
+
+if (!hasError){
+    console.log("não há erros")
+}
