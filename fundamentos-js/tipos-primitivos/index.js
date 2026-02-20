1787b6b1dba77f15ca3a6fdcc8575bee2f232345
Eduardo Kenzo <edukenzo428@gmail.com>
2026-02-20 15:50:56 -0300

    tipos primitivos

---
+++ fundamentos-js/tipos-primitivos/index.js
@@ -0,0 +1,30 @@
+//tipos primitivos
+
+//string- texto
+const nome = "Eduardo"
+
+//number
+const idade = 30
+const pi = 3.14
+
+//boolean
+const possuiFaculdade = true
+
+if (possuiFaculdade) {
+    console.log("Essa pessoa possui faculdade")
+} else {
+    console.log("Essa pessoa não possui faculdade")
+}
+
+//undefined não foi inicializado com algum valor
+let cor
+console.log(cor)
+
+const pessoa = { nome: "Eduardo", idade: 21}
+console.log("O nome da pessoa é:", pessoa.nome)
+console.log("O nome da pessoa é:", pessoa.idade)
+
+//null - não tem valor definido
+const avaliacao = null
+avaliacao = 4
+
