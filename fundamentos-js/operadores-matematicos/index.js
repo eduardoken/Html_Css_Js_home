4181c79b2d77134472580d7ba2bc01ac15cb1cb4
Eduardo Kenzo <edukenzo428@gmail.com>
2026-02-21 13:48:14 -0300

    operadores

---
+++ fundamentos-js/operadores-matematicos/index.js
@@ -0,0 +1,36 @@
+//soma
+// const numero1 = 2
+// const numero2 = 3
+
+// const soma = numero1 + numero2
+// console.log(soma)
+
+//subtração
+// const numero1 = 20
+// const numero2 = 3
+
+// const subtracao = numero1 - numero2
+// console.log(subtracao)
+
+//mutiplicação
+// const numero1 = 20
+// const numero2 = 3
+
+// const mutiplicacao = numero1 * numero2
+// console.log(mutiplicacao)
+
+//divisão
+// const numero1 = 6
+// const numero2 = 3
+
+// const divisao = numero1 / numero2
+// console.log(divisao)
+
+//resto da divisão
+const numero1 = 10
+const numero2 = 3
+
+const resto = numero1 % numero2
+console.log(resto)
+
+//
