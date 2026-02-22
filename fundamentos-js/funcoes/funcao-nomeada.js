594ca5749a52ea2d0de8042523b676dac78ca2f0
Eduardo Kenzo <edukenzo428@gmail.com>
2026-02-22 15:24:01 -0300

    funcao

---
+++ fundamentos-js/funcoes/funcao-nomeada.js
@@ -0,0 +1,7 @@
+// funções nomeadas
+function saudacao(nome, idade) {
+    return`Olá ${nome}. Você tem ${idade} anos`
+}
+
+const saudacaoEduardo = saudacao("Eduardo", 21)
+console.log(saudacaoEduardo)
