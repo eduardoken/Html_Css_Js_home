2e4829e58a086910b26687feaa768cc8d806029f
Eduardo Kenzo <edukenzo428@gmail.com>
2026-02-22 15:58:32 -0300

    array

---
+++ fundamentos-js/arrays-objetos/arrays/index.js
@@ -0,0 +1,18 @@
+const filmes = ["Homen-Aranha", "Batman", "Superman", "Mulher-Maravilha"]
+
+const array = ["Eduardo", 21, {cidade: "São Paulo"}]
+
+const arrayBidimensional = [
+    [1,2,3],
+    [3,4,5],
+    [6,7,6]
+]
+
+const arrayMultidimensional = [
+    ["Homen-Aranha",9],
+    ["Batman",7],
+    ["Superman",3],
+    ["Mulher-Maravilha",4.2,["Gal Gadot", 40]]
+]
+
+console.log(arrayMultidimensional[3][2][0])
