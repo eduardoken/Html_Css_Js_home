be2a30c034dee19a02184a6e4a8d3f48c5e2156b
Eduardo Kenzo <edukenzo428@gmail.com>
2026-02-23 18:08:27 -0300

    DOM

---
+++ fundamentos-js/eventos-e-dom/introducao-dom-seletores/index.js
@@ -0,0 +1,19 @@
+//getElementById - busca um elemento pelo atributo id
+
+// const button = document.getElementById("button")
+// console.log(button)
+
+// getElementByClassName - busca elementos pelo nome da classe
+
+// const h1 = document.getElementsByClassName("title")
+// console.log(h1)
+
+// querySelectorAll - busca todos os elementos que correspondem a um seletor CSS
+
+// const h1 = document.querySelectorAll(".title")
+// console.log(h1)
+
+// querySelector - busca o primeiro elemento que correspondem a um seletor CSS
+
+const p = document.querySelector(".paragrafo")
+p.textContent = "Novo texto do parágrafo"
