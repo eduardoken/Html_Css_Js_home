cda73d675391a204563d42174f14ef17d5bd7dc1
Eduardo Kenzo <edukenzo428@gmail.com>
2026-03-11 15:45:31 -0300

    async-await

--- js-promises/asinc-await/script.js
+++ js-promises/asinc-await/script.js
@@ -1,3 +1,30 @@
 const orderPizza = () => {
-    return new Promise((resolve))
+    return new Promise((resolve) => {
+        console.log("Pedido da pizza enviado para a cozinha")
+        
+        setTimeout(() => {
+            resolve("Pizza pronta!")
+        },3000)
+    })
 }
+
+//  simular uma api de entrega de pizza
+const deliverPizza = () => {
+    return new Promise((resolve) =>{
+        console.log("A pizza está a caminho...")
+
+        setTimeout(() => {
+            resolve("Pizza entregue")
+        },3000)
+    })
+}
+
+const makeOrder = async () => {
+    const pizzaMessage = await orderPizza()
+    console.log(pizzaMessage)
+
+    const deliveryMessage = await deliverPizza()
+    console.log(deliveryMessage)
+}
+
+makeOrder()
